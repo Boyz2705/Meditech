be710b06daf4d54aa1c2ce7d5b87e04e6b9f9b22
arifbucholi <muhammad.arif.ma23@gmail.com>
2023-07-17 23:23:01 +0700

    bar responTime & keaktifan pegawai

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -137,61 +137,29 @@
                 </div>
             </div>
         </div>
-
-        <br>
-
-        {{-- <div class="card"> --}}
-            <div class="main-panel" style="background:transparent;">
-                <div class="col-lg-6 grid-margin stretch-card">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">Bar chart</h4>
-                            <canvas id="barChart" style="height:230px"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        {{-- </div> --}}
-
-        {{-- <div class="card">
-            <div class="main-panel">
-                <div class="content-wrapper">
-                  <div class="page-header">
-                    <h3 class="page-title"> Chart-js </h3>
-                    <nav aria-label="breadcrumb">
-                      <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Charts</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Chart-js</li>
-                      </ol>
-                    </nav>
-                  </div>
-
-                </div>
-            </div>
-        </div> --}}
     </div>
 </div>
 
 <div class="row">
-    <div class="col-lg-6 grid-margin">
-        <div class="page-header">
-            <h3 class="page-title"> Chart Member Meditech </h3>
-            {{-- <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Charts</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Chart-js</li>
-                </ol>
-            </nav> --}}
-        </div>
-        <div class="row">
-            <div class="col-lg-12 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Member</h4>
-                        <canvas id="barChart" style="height:230px"></canvas>
-                    </div>
-                </div>
+    <div class="content-wrapper" style="padding:12px">
+      {{-- <div class="page-header">
+      </div> --}}
+      <div class="row">
+        <div class="col-lg-6 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">ResponTime Pegawai</h4>
+              <canvas id="lineChart" style="height:250px"></canvas>
             </div>
+          </div>
+        </div>
+        <div class="col-lg-6 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Keaktifan Pegawai</h4>
+              <canvas id="barChart" style="height:230px"></canvas>
+            </div>
+          </div>
         </div>
     </div>
 </div>
