f137a641a97ebd5b4bcbdeda248a33de751a1b9f
ameciii <salma.azaria-2021@vokasi.unair.ac.id>
2023-07-18 14:19:00 +0700

    keaktifan pegawai

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,7 +11,8 @@ class DashboardController extends Controller
     public function index(){
         return view('dashboard.index', [
             "title" => "Dashboard Meditech",
-            "pegawai" => User::where('roleid', 0)->paginate(10)
+            "pegawai" => User::where('roleid', 0)->paginate(10),
+            "member" => Member::all(),
 
         ]);
     }
